0b3e5b931bffac74775f13e31eb2d8056bbfb8b3
Bom Bk <bomncit@gmail.com>
2021-10-06 03:05:43 +0545

    userloginwithdb

---
+++ ecommerse/database/seeders/User_Seeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\facades\DB;
+use Illuminate\Support\facades\Hash;
+
+class User_Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert(
+            [
+                'name'=>'sam thapa',
+                'email'=>'sam@gmail.com',
+                'password'=>Hash::make('12345')
+            ]
+            );
+
+
+
+    }
+}
